1c56d75804e207c0555b51e08a5f110028366432
Mohammad <mohammad.alabed132@gmail.com>
2026-05-14 23:02:13 +0400

    fix file extensions

---
+++ app/layout.jsx
@@ -0,0 +1,18 @@
+import "./globals.css";
+import { Toaster } from "sonner";
+
+export const metadata = {
+  title: "Deal Drop",
+  description: "",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+        <Toaster richColor />
+      </body>
+    </html>
+  );
+}
